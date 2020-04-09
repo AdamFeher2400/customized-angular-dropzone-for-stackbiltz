27b4069b06a6d61dc63fe633b257556e10b2dec6
AdamFeher2400 <adamf2400@gmail.com>
2020-04-09 11:23:42 +0800

    0.2 finish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,4 +18,17 @@ export class AppComponent  {
 		console.log(event);
 		this.files.splice(this.files.indexOf(event), 1);
 	}
+
+  getFileSize(size) {
+    if(size < 1024) {
+      return size + 'B';
+    }
+    if(size < 1024 * 1024) {
+      return (size / 1024).toFixed() + 'KB';
+    }
+    if(size < 1024 * 1024 * 1024) {
+      return (size / 1024 / 1024).toFixed(2) + 'MB';
+    }
+    return (size / 1024 / 1024 / 1024).toFixed(2) + 'GB';
+  }
 }
